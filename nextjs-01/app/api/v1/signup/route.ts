28d644e56f00d1a9c4e9be268121c3d9e46929d2
Aditya raj <adityaraj987456@gmail.com>
2025-10-01 22:39:57 +0530

    loaders, Prisma ORM setup + migrations, API routes done.

--- nextjs-01/app/api/v1/signup/route.ts
+++ nextjs-01/app/api/v1/signup/route.ts
@@ -1,13 +1,23 @@
 import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from '@/app/generated/prisma';
 
+
+const prismaClient = new PrismaClient();    //instance of prisma client.
 export async function POST(req:NextRequest) {
 
     //here how req.body???  --> using nextrequest.
 
     const data=await req.json();
-    console.log(data)
-    console.log('backend msg-----------------------------'+ data.user)
+   // console.log(data)
+   // console.log('backend msg-----------------------------'+ data.user)
 
+   await prismaClient.user.create({
+    data:{
+        username: data.user,
+        email:data.ema,
+        password:data.pass
+    }
+   })
 
     return NextResponse.json({
         message:'You have signed up successfully !'
